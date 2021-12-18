caf3356329ac75faf46b20b07b0c6dc97dbe8a19
zhanglm <1160244583@qq.com>
2021-12-18 13:22:56 +0800

    '项目初始化-引入Vant组件库'

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入全局样式
 import '@/styles/index.less'
+// 引入vant组件库
+import Vant from 'vant'
+// 引入vant组件库样式
+import 'vant/lib/index.css'
 
+// 注册全局可用的vant
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
